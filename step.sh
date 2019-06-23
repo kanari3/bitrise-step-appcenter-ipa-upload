749f916d65d674c3dd2bd714bfa325ac49db2d4c
kanari3 <kanari333333@gmail.com>
2019-06-24 03:43:57 +0900

    AppCenter ipa upload 0.0.1

---
+++ step.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+# Ensure Microsoft AppCenter CLI installed
+if hash appcenter 2>/dev/null; then
+  echo "Microsoft AppCenter CLI already installed."
+else
+  echo "Microsoft AppCenter CLI is not installed. Installing..."
+  npm install -g appcenter-cli
+fi
+
+envman add --key APP_CENTER_APP_ID
+envman add --key APP_CENTER_ACCESS_TOKEN
+envman add --key APP_CENTER_DISTRIBUTION_GROUP
+
+appcenter distribute release -f "${BITRISE_IPA_PATH}" -r "bitrise build" -a "${APP_CENTER_APP_ID}" --token "${APP_CENTER_ACCESS_TOKEN}" -g "${APP_CENTER_DISTRIBUTION_GROUP}" --quiet
+
+exit 0
