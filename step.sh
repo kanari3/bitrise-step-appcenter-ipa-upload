c371f1a0e3eb02e7d52687c5080bf58623aaa97c
kanari3 <kanari333333@gmail.com>
2021-10-07 03:37:38 +0900

    suport optinal build message

--- step.sh
+++ step.sh
@@ -14,6 +14,10 @@ envman add --key APP_CENTER_ACCESS_TOKEN
 envman add --key APP_CENTER_DISTRIBUTION_GROUP
 envman add --key APP_CENTER_BUILD_MESSAGE
 
+if [ -z "${APP_CENTER_BUILD_MESSAGE}" ] ; then
+  APP_CENTER_BUILD_MESSAGE="no message"
+fi
+
 appcenter distribute release -f "${BITRISE_IPA_PATH}" -r "${APP_CENTER_BUILD_MESSAGE}" -a "${APP_CENTER_APP_ID}" --token "${APP_CENTER_ACCESS_TOKEN}" -g "${APP_CENTER_DISTRIBUTION_GROUP}" --quiet
 
 exit 0
